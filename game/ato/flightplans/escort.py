709c1652244037e70217ac328645309a99d8517f
Dragonius <Blackerhawker@hotmail.com>
2023-03-29 13:14:19 +0300

    Fixed escord

--- game/ato/flightplans/escort.py
+++ game/ato/flightplans/escort.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import Type
 
+from dcs.mapping import Point, Vector2
+
 from .formationattack import (
     FormationAttackBuilder,
     FormationAttackFlightPlan,
@@ -23,56 +25,13 @@ class Builder(FormationAttackBuilder[EscortFlightPlan, FormationAttackLayout]):
     def layout(self) -> FormationAttackLayout:
         assert self.package.waypoints is not None
 
-        location = self.package.target
         builder = WaypointBuilder(self.flight, self.coalition)
-        if isinstance(location, FrontLine):
-            from game.missiongenerator.frontlineconflictdescription import (
-                FrontLineConflictDescription,
-            )
-
-            (
-                ingress_point,
-                heading,
-                distance,
-            ) = FrontLineConflictDescription.frontline_vector(
-                location, self.theater, self.coalition.game.settings
-            )
-            target_point = ingress_point.point_from_heading(
-                heading.degrees, distance / 2
-            )
-            egress_point = ingress_point.point_from_heading(heading.degrees, distance)
-
-            ingress_distance = ingress_point.distance_to_point(
-                self.flight.departure.position
-            )
-            egress_distance = egress_point.distance_to_point(
-                self.flight.departure.position
-            )
-            if egress_distance < ingress_distance:
-                ingress_point, egress_point = egress_point, ingress_point
-
-            ingress = builder.ingress(
-                FlightWaypointType.INGRESS_ESCORT, ingress_point, location
-            )
-            target = FlightWaypoint(
-                self.package.target.name,
-                FlightWaypointType.TARGET_POINT,
-                target_point,
-                self.doctrine.ingress_altitude * 0.5,
-                description=self.package.target.name,
-                pretty_name=self.package.target.name,
-            )
-            # egress = builder.egress(egress_point, location)
-            split = builder.split(egress_point)
-        else:
-
-            ingress, target = builder.escort(
-                self.package.waypoints.ingress, self.package.target
-            )
-            split = builder.split(self.package.waypoints.split)
-
+        ingress, target = builder.escort(
+            self.package.waypoints.ingress, self.package.target
+        )
         hold = builder.hold(self._hold_point())
         join = builder.join(self.package.waypoints.join)
+        split = builder.split(self.package.waypoints.split)
         refuel = builder.refuel(self.package.waypoints.refuel)
 
         return FormationAttackLayout(
