3dbf3c0b18ac752d637668453c3f11c2c0a5ea92
zhexu14 <64713351+zhexu14@users.noreply.github.com>
2023-04-23 03:50:20 +1000

    Add missing argument when fixing TOTs.
    Part of https://github.com/dcs-liberation/dcs_liberation/issues/2746.
    
    (cherry picked from commit 1b725988033c47c9f1d8e9414e6eb8c159dc5b55)

--- qt_ui/widgets/QTopPanel.py
+++ qt_ui/widgets/QTopPanel.py
@@ -176,10 +176,10 @@ class QTopPanel(QFrame):
         return packages
 
     @staticmethod
-    def fix_tots(packages: List[Package]) -> None:
+    def fix_tots(packages: List[Package], now: datetime) -> None:
         for package in packages:
             estimator = TotEstimator(package)
-            package.time_over_target = estimator.earliest_tot()
+            package.time_over_target = estimator.earliest_tot(now)
 
     def ato_has_clients(self) -> bool:
         for package in self.game.blue.ato.packages:
@@ -242,7 +242,7 @@ class QTopPanel(QFrame):
         mbox.exec_()
         clicked = mbox.clickedButton()
         if clicked == auto:
-            self.fix_tots(negative_starts)
+            self.fix_tots(negative_starts, self.sim_controller.current_time_in_sim)
             return True
         elif clicked == ignore:
             return True
