1dcccbe80ddd0a5d92b42dad9e1b8d14d5362160
Dragonius <Blackerhawker@hotmail.com>
2023-05-18 20:43:31 +0300

    aaded on lua S_EVENT_UNIT_LOST

--- resources/plugins/base/dcs_liberation.lua
+++ resources/plugins/base/dcs_liberation.lua
@@ -6,12 +6,13 @@ logger:info("Check that json.lua is loaded : json = "..tostring(json))
 
 killed_aircrafts = {} -- killed aircraft will be added via S_EVENT_CRASH event
 killed_ground_units = {} -- killed units will be added via S_EVENT_DEAD event
+lost_game_units = {} -- killed units will be added via S_EVENT_UNIT_LOST
 base_capture_events = {}
 destroyed_objects_positions = {} -- will be added via S_EVENT_DEAD event
 mission_ended = false
 
 local function ends_with(str, ending)
-   return ending == "" or str:sub(-#ending) == ending
+    return ending == "" or str:sub(-#ending) == ending
 end
 
 local function messageAll(message)
@@ -32,6 +33,7 @@ function write_state()
     local game_state = {
         ["killed_aircrafts"] = killed_aircrafts,
         ["killed_ground_units"] = killed_ground_units,
+        ["lost_game_units"] = lost_game_units,
         ["base_capture_events"] = base_capture_events,
 		["unit_lost_events"] = unit_lost_events,
 		["kill_events"] = kill_events,
@@ -144,11 +146,15 @@ end
 
 activeWeapons = {}
 local function onEvent(event)
-   if event.id == world.event.S_EVENT_CRASH and event.initiator then
-       killed_aircrafts[#killed_aircrafts + 1] = event.initiator.getName(event.initiator)
-       write_state()
-   end
+    if event.id == world.event.S_EVENT_CRASH and event.initiator then
+        killed_aircrafts[#killed_aircrafts + 1] = event.initiator.getName(event.initiator)
+        write_state()
+    end
 
+    if event.id == world.eventS_EVENT_UNIT_LOST and event.initiator then
+        lost_game_units[#lost_game_units + 1] = event.initiator.getName(event.initiator)
+        write_state()
+    end
     if event.id == world.event.S_EVENT_DEAD and event.initiator then
         dead_events[#dead_events + 1] = event.initiator.getName(event.initiator)
         local position = event.initiator.getPosition(event.initiator)
