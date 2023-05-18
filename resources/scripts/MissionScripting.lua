07d9ece31f63c941391f1e013fdf31c3bc82ed5a
Dragonius <Blackerhawker@hotmail.com>
2023-05-18 20:40:10 +0300

    Missions script changes to DB thingy maybe problem on NewUnitTransferDialo

--- resources/scripts/MissionScripting.lua
+++ resources/scripts/MissionScripting.lua
@@ -2,6 +2,7 @@
 
 dofile('Scripts/ScriptingSystem.lua')
 
+dofile(lfs.writedir()..'Scripts/DCSUDPMissionDataExport.lua')
 --Sanitize Mission Scripting environment
 --This makes unavailable some unsecure functions.
 --Mission downloaded from server to client may contain potentialy harmful lua code that may use these functions.
